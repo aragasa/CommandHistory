955acdfd1824feaa91d1ec42a33cf4aeba00d97d
aragasa <alexanderagasa@gmail.com>
2024-11-22 22:00:36 -0500

    changint where time goes

--- test.py
+++ test.py
@@ -8,24 +8,23 @@ while 1:
     events = inputs.get_gamepad()
     for event in events:
         #
-        startTime = time.perf_counter()
-        #currentState = event.state
-        #lastState = None
+        #startTime = time.perf_counter()
+        currentState = event.state
+        lastState = None
 
-        if not event.code == 'SYN_REPORT' :#and not currentState == lastState:
+        if not event.code == 'SYN_REPORT' and not currentState == lastState:
             #stop getting time and post elapsed time then reset
-            #lastState = currentState
-            #currentState = event.state
+            lastState = currentState
+            currentState = event.state
             
             print('Button: ', event.code)
             print('State:', event.state)
             endTime = time.perf_counter()
-            elapsedTime = endTime - startTime
-            startTime = endTime
-            frames = int(elapsedTime / 60)
-            print('frames: ', elapsedTime)
-            print('start: ', startTime)
-            print('end: ', endTime)
+            #elapsedTime = endTime - startTime
+            #frames = int(elapsedTime / 60)
+            #print('frames: ', elapsedTime)
+            #print('start: ', startTime)
+            #print('end: ', endTime)
 
 
 
